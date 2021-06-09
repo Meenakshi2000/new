62e30a4780cf626d0655eeb4245321e36525d3ec
Meenakshi2000 <44131450+Meenakshi2000@users.noreply.github.com>
2021-06-09 12:38:47 +0530

    Add files via upload

---
+++ song.js
@@ -0,0 +1,58 @@
+
+let songs = [ { Id: 0,
+    
+        name: "Ande ka fanda",
+        movie: "Jodi No. 1 " ,
+        path: "./02. Andde Ka Funda.mp3"
+       
+        
+    },
+  { Id: 1,
+    name: "Jaane Kyon",
+        movie: "Dil Chahta Hai",
+        path: "https://pagalsong.in/uploads/systemuploads/mp3/Dil Chahta Hai/Jaane Kyon Log Pyar.mp3"
+    },
+  
+  { Id: 2,
+    name: "Bade Miyan",
+    movie: "Bade Miyan Chote Miyan",
+    path: "https://pagalsong.in/uploads/systemuploads/mp3/Bade Miyan Chote Miyan/Bade Miyan Toh Bade Miyan.mp3",
+},
+  { Id: 3,
+    name: "Pyar Ka Dushman",
+    movie: "Amar Akbar Anthony",
+    path: " https://jatt.download/mrisky/dren/data2/Hindi_Movies/201510/Amar_Akbar_Anthony/128/Taiyabali_Pyar_Ka_Dushman.mp3/Taiyabali Pyar Ka Dushman.mp3",
+},
+  { Id: 4,
+    name: "Main Jaadugar",
+        movie: "Jaadugar",
+        path: "https://jatt.download/dren/music/data/Hindi_Movies/201403/Jaadugar/48/Main_Jaadugar_3.mp3/Main Jaadugar 3.mp3",
+    },
+  { Id: 5,
+    name: "Paanch Rupaiya Baara Aana",
+    movie: "Chalti Ka Naam Gaadi",
+    path: "https://pagalworld.casa/download.php?id=9981.mp3",
+},
+  { Id: 6,
+    name: "Churalo Na Dil Mera",
+    movie: "Kareeb",
+ 
+    path: "https://pagalfree.com/musics/128-Churalo Na Dil Mera - Kareeb 128 Kbps.mp3",
+},
+  { Id: 7,
+    name: "Bombay Se Aaya Mera Dost",
+    movie: "Aap Ki Khatir",
+    path: "./songs/Bambai-Se-Aaya-Mera-Dost-Bappi-Lahiri-(pagalworldsongs.co.in).mp3",
+},
+  { Id: 8,
+    name: "Padosan Apni Murgi",
+        movie: "Jaadugar",
+        path: "  https://jatt.download/music/data/Hindi_Movies/201403/Jaadugar/128/Padosan_Apni_Murgi_Ko_Rakhna_5.mp3",
+    },
+    { Id: 9,
+        name: "Coolie No.1",
+        movie: "Coolie No.1",
+        path: "https://pagalfree.com/musics/128-Coolie No.1 - Coolie No.1 128 Kbps.mp3",
+      
+    }
+  ];
